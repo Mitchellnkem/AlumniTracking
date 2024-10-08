7a1372913cb48f26e108c0315b8d2c10e55e36c0
Mitchellnkem <Mitchellnkemgmailcomgit config --global user.email Mitchellnkemgmail.com>
2024-10-08 11:22:48 +0000

    first commit today

--- general.php
+++ general.php
@@ -3,152 +3,176 @@
 		<tr>
 			<td align="left">
 				<div class="form_tab">
-					Employed Questions
+					General Information
 				</div>
 				<fieldset>
 					<center>
-						<table cellspacing="6" cellpadding="4" width="100%">
-							<td>
-								<tr>
-									<td align="right" width="45%">
-										Are you currently employed? :
+						<table cellspacing="5" cellpadding="4" width="100%">
 
-									</td>
-									<td align="left" width="55%">
-										<input name="employed" type="radio" value= "Yes" >Yes
-										<input name="employed" type="radio" value= "No" >No
-									</td>
-								</tr>
+							<table cellspacing="5" cellpadding="4" width="100%">
+								<tbody>
+									<tr>
+										<td align="right" width="45%">
+											Full Name:
+										</td>
 
-								<tr>
-									<td align="right" width="45%">
-										Please indicate below your employmen hsitory starting from current (includes governmental and private Organization) :
-									</td>
-									<td align="left" width="55%">
-										<table class="table" border="1">
-											<th>
-												<tr>
-													<th>Name of Employer / Agency</th>
-													<th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Date &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
-													<th>Status</th>
-													<th>Position</th>
-												</tr>
-											</thead>
-											<tbody>
-												<tr class="odd">
-													<td contenteditable="true"></td>
-													<td id="" contenteditable="true"></td>
-													<td id="" contenteditable="true"></td>
-													<td id="" contenteditable="true"></td>
-												</tr>
-												<tr>
-													<td contenteditable="true"></td>
-													<td id="" contenteditable="true"></td>
-													<td id="" contenteditable="true"></td>
-													<td id="" contenteditable="true"></td>
-												</tr>
-												<tr class="odd">
-													<td contenteditable="true"></td>
-													<td id="" contenteditable="true"></td>
-													<td id="" contenteditable="true"></td>
-													<td id="" contenteditable="true"></td>
-												</tr>
-												<tr class="odd">
-													<td>Employment Status :  <br/>
-														(a) Regulaur or Permanent <br/> (b) Contract <br/> (c) Temporary <br/> (d) Casual <br/> (e) On job order
+										<td align="left" width="55%">
+											<input type="text" name="fullname" maxlength="46" required>
+										</td> 
+									</tr>
 
-													</td>
-													<td></td>
-													<td></td>
-													<td></td>
-												</tr>
-											</tbody>
-										</table>
-									</td>
-								</tr>
-							</td>
+									<tr>
+										<td align="right" width="45%">
+											 Address:
+										</td>
+										<td align="left" width="55%">
+											<input type="text" name="paddress" maxlength="46" required>
+										</td>
+									</tr>
 
+									<tr>
+                                        <td align="right" width="45%">
+                                            Email Address :
+                                        </td>
+                                        <td align="left" width="55%">
+											<input name="eaddress" type="text" maxlength="46" required>
+                                         </td>
+									</tr>
+																			
+									<tr>
+                                        <td align="right" width="45%">
+                                        	Telephone or Contact Number(s) :
+                                        </td>
+                                        <td align="left" width="55%">
+											<input name="telephone" type="text" maxlength="46" required>
+                                        </td>
+									</tr>
+																			
+																			
+									
+                                    <tr>
+                                        <td align="right">
+                                             Civil Status :
+                                        </td>
+                                        <td align="left">
+                                        	 <select name="cs" required>
+													<option  value="">--Select--</option>
+													<option value="Single">Single</option>
+													<option value="Married">Married</option>
+													<option value="Separeted">Separated</option>
+													<option value="Divorced">Divorced</option>
+													<option value="Single Parent">Single Parent</option>
+													<option value="Widow or Widower">Widow or Widower</option>
+
+											</select>
+                                        </td>
+                                    </tr>
+									
+									<tr>
+                                        <td align="right" width="45%">
+                                            Sex / Gender :
+                                        </td>
+                                        <td align="left" width="55%">
+											<input name="sex" type="radio" value = "Male" maxlength="46" required>Male &nbsp;
+											<input name="sex" type="radio" value = "Female" maxlength="46" required>Female &nbsp;
+											<input name="sex" type="radio" value = "Rather not say" maxlength="46" required>Rather not say &nbsp;
+                                        </td>
+									</tr>
+									
+									<tr>
+										<td align="right" width="45%">
+                                            Date of Birth :
+                                        </td>
+                                        <td align="left" width="55%">
+                                            <input type="date" placeholder="YYYY-MM-DD" name="dob" required>
+                                        </td>
+									</tr>
+									
+									<tr>
+                                        <td align="right">
+                                            Religous Affiliation :
+                                        </td>
+                                        
+										<td align="left">
+                                            <select name="Religous" required>
+												<option  value="">--Select--</option>
+												<option value="Roman Catholic">Roman Catholic</option>
+												<option value="Iglesia Ni Cristo">Christian</option>
+												<option value="Methodist">Methodist</option>
+												<option value="Jehovah's Witnesses">Jehovah's Witnesses</option>
+												<option value="Born Again Christian">Born Again Christian</option>
+												<option value="Born Again Christian">Muslim</option>
+												<option value="Others">Others</option>
+										
+											</select>
+                                        </td>
+                                    </tr>
+									
+									<tr>
+                                        <td align="right">
+                                            Region of Origin :
+                                        </td>
+                                        
+										<td align="left">
+                                            <select name="Region" required>
+												<option  value="">--Select--</option>
+												<option value="Region 1">Region 1</option>
+												<option value="Region 2">Region 2</option>
+												<option value="Region 3">Region 3</option>
+												<option value="Region 4">Region 4</option>
+												<option value="Region 5">Region 5</option>
+												<option value="Region 6">Region 6</option>
+												<option value="Region 7">Region 7</option>
+												<option value="Region 8">Region 8</option>
+												<option value="Region 9">Region 9</option>
+												<option value="Region 10">Region 10</option>
+												<option value="Region 11">Region 11</option>
+												<option value="Region 12">Region 12</option>
+												<option value="Region 13">Region 13</option>
+												<option value="CAR">CAR</option>
+												<option value="ARMM">ARMM</option>
+												<option value="CARAGA">CARAGA</option>
+											</select>
+                                        </td>
+                                    </tr>
+									
+									<tr>
+                                        <td align="right" width="45%">
+                                            Province :
+                                        </td>
+                                        
+										<td align="left" width="55%">
+											<input name="province" type="text" maxlength="46" required>
+                                        </td>
+									</tr>
+																			
+									<tr>
+                                        <td align="right" width="45%">
+                                            Location of Residence :
+                                        </td>
+                                        
+										<td align="left" width="55%">
+											<select name="Residence" required>
+												<option  value="">--Select--</option>
+												<option value="Urban">Urban</option>
+												<option value="Rural">Rural</option>
+												<option value="Rurban">Rurban</option>
+																										
+											</select>
+                                        </td>
+									</tr>
+																			
+                                                                    
+								</tbody>
+							</table>
+                                           
+                                                                              
+                                                                            
+
+
+								
+							
 						</table>
-
-															<tbody>	
-																<tr>
-																	<td align="right" width="45%" > 
-																		What type of Organization do you belong?
-																	</td> 
-																	<td align="left" width="55%" >
-																		<select name="organization" id="" required>
-																			<option value="">--Please Select--</option>
-																			<option value="Governmental Organization">Governmental Organization (GO)</option>
-																			<option value="Business Organization">Business Organization (BO)</option>
-																			<option value="Private Organization">Private Organization</option>
-																			<option value="Cooperative">Cooperative</option>
-																			<option value="Non-Governmental Organization">Non-Governmental Organzation</option>
-																			<option value="Peoples Organization">Peoples Organization</option>
-																		</select> <br/>
-																	</td>
-																</tr>
-
-																<tr><br/></tr>
-
-																<tr>
-																	<td align="right">
-																		Were you selected according to your qualification(s)?
-																	</td>
-
-																	<td align="left">
-																		<select name="selection" id="" required>
-																			<option value="">--Please Select--</option>
-																			<option value="Selection">Selection</option>
-																			<option value="Retention">Retention</option>
-																			<option value="Promotion">Promotion</option>
-																		</select> <br/>
-																	</td>
-																</tr>
-
-																<tr><br/></tr>
-
-																<tr> 
-
-																	<td align="right" width="45%">
-																		What is your annual income from your present employment? $Usd :
-																	</td>
-
-																	<td align="left" width="55%">
-																		<input type="text" name="income">
-																	</td>
-																</tr>
-
-																<tr><br/></tr>
-																	<tr>
-																		<td align="right" width="45%">
-																			Were you able to use the skills acquired from your degree/course to your current work? :
-																		</td>
-																		<td align="left" width="55%">
-																			<select name="skills" id="" required>
-																				<option value="">--Please Select</option>
-																				<option value="Yes">Yes</option>
-																				<option value="No">No</option>
-																			</select><br/>
-																		</td>
-																	</tr>
-
-															</tbody>
-
-														</table>
-
-														<br>
-														<center>
-															<table cellspacing="0" width="80%" border="0">
-																<tbody>
-																	<tr>
-																		<td align="center">
-																			<input type="submit" name="submit" id="submit" class="sub_red">
-																		</td>
-																	</tr>
-																</tbody>
-															</table>
-														</center>
-
 					</center>
 				</fieldset>
 				
